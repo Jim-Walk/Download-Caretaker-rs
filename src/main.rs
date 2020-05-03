b7b87df565bf8feb19c58d4a8edbb25848a26e71
andrewpsuedonym <j.m.walker@live.co.uk>
2020-05-03 21:48:36 +0100

    finalise strutcure

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 extern crate dirs;
 use std::time::SystemTime;
-use std::fs;
+use std::path::Path;
+use std::{io, fs, process};
 
-fn delete_or_move_file(&Path
+fn delete_or_move_file(p: &Path){
+    println!("{}", p.display());
+    println!("Move to (d)ocuments, (m)usic, (p)ictures, mo(v)ies or (o)pen");
+    println!("You can (a)bort at any time");
+
+    let mut input = String::new();
+    io::stdin()
+       .read_line(&mut input)
+       .expect("Failed to read line");
+
+   match input.trim().as_ref() {
+       "d" => println!("moved to documents"),
+       "a" => process::exit(0x0100),
+       _ => println!("idk")
+   }
+}
 
 fn main() {
     println!("Download Caretaker Rust");
@@ -15,7 +31,7 @@ fn main() {
             // If file is older than a month, delete it
             if let Ok(file_age) = SystemTime::now().duration_since(file_modified){
                 if file_age.as_secs() > 60 * 60 * 24 * 7 {
-                    println!("{} older than a week", path.display());
+                    delete_or_move_file(&path);
                 }
             }
         }
