4128933dc75e00f02b6616e8bd26f71a3fc6fd59
andrewpsuedonym <j.m.walker@live.co.uk>
2020-05-17 21:54:17 +0100

    Add remove and bat

--- src/main.rs
+++ src/main.rs
@@ -20,18 +20,31 @@ fn move_file(src: &Path, dest_folder: &Path) {
         fs::rename(src, new_f_name).unwrap();
     }
     println!("Moved to {}", dest_folder.to_str().unwrap());
+    println!("-------------------------------------------")
+}
+
+fn remove_file_or_dir(src: &Path){
+    let metadata = fs::metadata(&src).unwrap();
+    if metadata.is_dir() {
+        fs::remove_dir_all(src).unwrap();
+    } else {
+       fs::remove_file(src).unwrap();
+    }
+
+    println!("-------------------------------------------")
 }
 
 fn bat_file(src: &Path) {
     PrettyPrinter::new().input_file(src)
                         .line_ranges(LineRanges::from(vec![LineRange::new(0,15)]))
                         .print().unwrap();
+    println!("-------------------------------------------")
 }
 
 fn delete_or_move_file(p: &Path) {
     println!("{}", trim_path(p));
     println!("Move to (d)ocuments, (m)usic, (p)ictures, (v)ideos/mo(v)ies");
-    println!("You can also (b)at the file, or (o)pen it");
+    println!("You can also (b)at the file, (o)pen or (r)emove it");
     println!("You can (a)bort at any time");
 
     let mut input = String::new();
@@ -44,9 +57,18 @@ fn delete_or_move_file(p: &Path) {
        "m" => move_file(p, &dirs::audio_dir().unwrap()),
        "p" => move_file(p, &dirs::picture_dir().unwrap()),
        "v" => move_file(p, &dirs::video_dir().unwrap()),
+       "r" => remove_file_or_dir(p),
        "a" => process::exit(0x0100),
-       "o" => {process::Command::new("open").arg(p).output().expect("unable to open file, this command only works on Mac");},
-       "b" => bat_file(p),
+       "b" => {
+                bat_file(p); 
+                delete_or_move_file(p);
+              },
+       "o" => {
+                process::Command::new("open")
+                                     .arg(p)
+                                     .output()
+                                     .expect("unable to open file, this command only works on Mac");
+              },
         _  =>   println!("Skipped File")
    }
 }
